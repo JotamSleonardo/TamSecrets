52d7c773a1ed634fc459a7c25d3def9b692b6a3a
jotam leonardo <jotam.leonardo@ffuf.de>
2026-01-23 14:48:29 +0800

    Initial Commit

---
+++ TamSecrets/TamSecretsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TamSecretsApp.swift
+//  TamSecrets
+//
+//  Created by Jotam Leonardo on 1/23/26.
+//
+
+import SwiftUI
+
+@main
+struct TamSecretsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
